aa2eaa443c88a2c1c4fa737230bd3bbc62e4502a
Julius Busecke <jbusecke@princeton.edu>
2019-10-18 03:37:49 +0000

    nightly updates

--- cmip6_preprocessing/parse_static_metrics.py
+++ cmip6_preprocessing/parse_static_metrics.py
@@ -1,5 +1,7 @@
+from .preprocessing import rename_cmip6
 import warnings
-def extract_all_availabe_metrics(col, grid_label, source_id, varname='areacello', preprocess=None):
+
+def extract_all_availabe_metrics(col, grid_label, source_id, varname='areacello', **kwargs):
     #Find an area for as many models as possible (no matter in which scenario, ensemble etc)
     subset = col.search(variable_id=[varname], grid_label='gn', source_id=source_id)
 
@@ -7,7 +9,8 @@ def extract_all_availabe_metrics(col, grid_label, source_id, varname='areacello'
     df = subset.df.drop_duplicates(subset='source_id')
     subset.df = df
     
-    return subset.to_dataset_dict(preprocess=preprocess)
+#     return subset.to_dataset_dict(preprocess=preprocess)# activate this when intake_esm is working
+    return subset.to_dataset_dict(**kwargs)
 
 def extract_static_metric(col, grid_label, source_id, varname='areacello', preprocess=None):
     #Find an area for as many models as possible (no matter in which scenario, ensemble etc)
@@ -28,11 +31,15 @@ def extract_static_metric(col, grid_label, source_id, varname='areacello', prepr
     else:
         return metric_dict[list(metric_dict.keys())[0]]
     
-def parse_metrics(data_dict, col, varname='areacello', preprocess=None):
+def parse_metrics(data_dict, col, varname='areacello', preprocess=None, rename=False):
+    """parse matching static metrics for each element of `data_dict`. 
+    preprocessing shouls eventually eliminate renam...once the """
     data_dict_parsed = {}
     for k, ds in data_dict.items():
         metric = extract_static_metric(col, ds.attrs['grid_label'], ds.attrs['source_id'])
         if not metric is None:
+            if rename:
+                metric = rename_cmip6(metric)
             # strip all coords and attributes from metric
             metric = metric.squeeze().reset_coords(drop=True)
             ds.coords[varname] = metric[varname]
