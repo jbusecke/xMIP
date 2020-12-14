9eeae19e305cad457fbc29d2b66fff6f3f437138
jbusecke <julius@ldeo.columbia.edu>
2020-12-14 10:35:03 -0500

    Properly deactivate the sep cloud tests

--- cmip6_preprocessing/tests/test_preprocessing_cloud_simple.py
+++ cmip6_preprocessing/tests/test_preprocessing_cloud_simple.py
@@ -1,6 +1,10 @@
 # This module tests data directly from the pangeo google cloud storage.
 # Tests are meant to be more high level and also serve to document known problems (see skip statements).
 import pytest
+
+pytest.skip("Deactivate sep test for now")
+pytest.importorskip("gcsfs")
+
 import xarray as xr
 import numpy as np
 from cmip6_preprocessing.tests.cloud_test_utils import (
@@ -13,8 +17,6 @@ from cmip6_preprocessing.tests.cloud_test_utils import (
 from cmip6_preprocessing.preprocessing import combined_preprocessing
 from cmip6_preprocessing.grids import combine_staggered_grid
 
-pytest.skip("Deactivate sep test for now")
-pytest.importorskip("gcsfs")
 
 print(f"\n\n\n\n$$$$$$$ All available models: {all_models()}$$$$$$$\n\n\n\n")
 
