0c78f22250b1344615f5ffcadddf59c783350d4f
Julius Busecke <julius@ldeo.columbia.edu>
2020-12-23 23:20:53 -0500

    Some more fixes

--- cmip6_preprocessing/tests/conftest.py
+++ cmip6_preprocessing/tests/conftest.py
@@ -5,4 +5,3 @@ def pytest_addoption(parser):
     parser.addoption("--variable_id_global", action="store", default="thetao")
     parser.addoption("--grid_label_global", action="store", default="gn")
     parser.addoption("--experiment_id_global", action="store", default="historical")
-    # parser.addoption("--experiement", action="store", default="historical")
