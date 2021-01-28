07cfbba46c6ed63f8ba7d00b2708cabc0b28d251
Julius Busecke <julius@ldeo.columbia.edu>
2021-01-28 16:15:10 -0500

    Reactivate all models

--- cmip6_preprocessing/tests/test_preprocessing_cloud.py
+++ cmip6_preprocessing/tests/test_preprocessing_cloud.py
@@ -16,8 +16,8 @@ from cmip6_preprocessing.grids import combine_staggered_grid
 
 pytest.importorskip("gcsfs")
 
-# test_models = all_models()
-test_models = ["CESM2-FV", "GFDL-ESM4"]
+test_models = all_models()
+# test_models = ["CESM2-FV", "GFDL-ESM4"]
 
 # does this work?
 def pytest_generate_tests(metafunc):
