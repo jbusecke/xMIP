25bdf346c8b383d57e31213a14db7554f37eb205
Julius Busecke <julius@ldeo.columbia.edu>
2021-01-28 12:34:19 -0500

    Rename command line flags

--- cmip6_preprocessing/tests/conftest.py
+++ cmip6_preprocessing/tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--variable_id_global", action="store", default="thetao")
-    parser.addoption("--grid_label_global", action="store", default="gn")
-    parser.addoption("--experiment_id_global", action="store", default="historical")
+    parser.addoption("--vi", action="store", default="thetao")
+    parser.addoption("--gl", action="store", default="gn")
+    parser.addoption("--ei", action="store", default="historical")
