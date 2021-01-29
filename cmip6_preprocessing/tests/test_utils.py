7d373a7ce53f534d190a376ac9f98bd42020180a
Julius Busecke <julius@ldeo.columbia.edu>
2021-01-29 15:53:53 +0000

    formatting with black

--- cmip6_preprocessing/tests/test_utils.py
+++ cmip6_preprocessing/tests/test_utils.py
@@ -1,5 +1,6 @@
 from cmip6_preprocessing.utils import google_cmip_col
 
+
 def test_google_cmip_col():
     col = google_cmip_col()
-    assert col.catalog_file == 'https://storage.googleapis.com/cmip6/pangeo-cmip6.csv'
+    assert col.catalog_file == "https://storage.googleapis.com/cmip6/pangeo-cmip6.csv"
--- cmip6_preprocessing/utils.py
+++ cmip6_preprocessing/utils.py
@@ -1,5 +1,6 @@
 import intake
 
+
 def google_cmip_col():
     """A tiny utility function to point to the 'official' pangeo cmip6 cloud files."""
     return intake.open_esm_datastore(
