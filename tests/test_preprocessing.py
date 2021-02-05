5c7585a0baaee84b1f812856beff4c78678b23ec
Julius Busecke <julius@ldeo.columbia.edu>
2021-02-05 13:00:44 -0500

    Remove dummy fail test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -42,10 +42,6 @@ def create_test_ds(xname, yname, zname, xlen, ylen, zlen):
     return ds
 
 
-def test_dummy_fail():
-    assert 1 == 0
-
-
 @pytest.mark.parametrize("xname", ["i", "x", "lon"])
 @pytest.mark.parametrize("yname", ["j", "y", "lat"])
 @pytest.mark.parametrize("zname", ["lev", "olev", "olevel"])
