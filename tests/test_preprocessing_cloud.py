e6b1fad116ea2c015f0468a7ad5a096c6bffb47a
Julius Busecke <julius@ldeo.columbia.edu>
2021-02-05 12:04:55 -0500

    more nmore

--- tests/test_preprocessing_cloud.py
+++ tests/test_preprocessing_cloud.py
@@ -142,19 +142,13 @@ intake_concat_failures = [
         "*",
         "gn",
     ),  # The bounds are slightly larger than 360. Should be easy to fix
-    (
-        "MIROC6",
-        ["uo"],
-        ["historical"],
-        "gn",
-    ),
     # (
     #     "MIROC-ES2L",
     #     ["uo"],
     #     "*",
     #     "gn",
     # ),  # The bounds are slightly larger than 360. Should be easy to fix
-    (
+    [uo - gn - ssp585 - GISS - E2 - 1 - G](
         "GISS-E2-1-G-CC",
         ["o2", "uo"],
         "*",
@@ -316,11 +310,17 @@ def spec_check_bounds_verticies(request, gl, vi, ei):
         + [
             ("FGOALS-f3-L", ["thetao", "so", "uo"], "*", "gn"),
             ("FGOALS-g3", ["thetao", "so", "uo"], "*", "gn"),
+            (
+                "MIROC6",
+                ["uo"],
+                ["historical"],
+                "gn",
+            ),
             ("NorESM2-MM", "thetao", "historical", "gn"),
             ("NorESM2-MM", ["thetao", "so"], "historical", "gr"),
             ("IPSL-CM6A-LR", ["thetao", "o2"], "historical", "gn"),
             ("NESM3", "uo", "ssp585", "gn"),
-            ("GFDL-CM4", "uo", "ssp585", "gn"),
+            ("GFDL-CM4", "uo", "*", "gn"),
             ("EC-Earth3", "uo", "ssp585", "gn"),
             ("EC-Earth3-Veg", "uo", "ssp585", "gn"),
         ]
@@ -408,13 +408,13 @@ def spec_check_grid(request, gl, vi, ei):
             ("FGOALS-f3-L", "*", "*", "gn"),
             ("FGOALS-g3", "*", "*", "gn"),
             ("E3SM-1-0", ["so", "thetao", "o2"], "*", "gn"),
-            # ("EC-Earth3", ["uo"], "*", "gn"),
+            ("EC-Earth3-AerChem", ["so"], "historical", "gn"),
             # ("EC-Earth3-Veg", ["uo"], "*", "gn"),
             ("MPI-ESM-1-2-HAM", "*", "*", "gn"),
             ("NorESM2-MM", "*", "historical", "gn"),
             ("NorESM2-MM", ["thetao", "so", "uo"], "historical", "gr"),
             ("IITM-ESM", "*", "*", "gn"),
-            ("GFDL-CM4", ["uo"], ["ssp585"], "gn"),
+            ("GFDL-CM4", ["uo"], "*", "gn"),
         ]
     )
     spec = (request.param, vi, ei, gl)
