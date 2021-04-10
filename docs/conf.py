12a1e1878f67bc9a5b86ef24d76bc64e5a59b21f
Julius Busecke <julius@ldeo.columbia.edu>
2021-04-10 12:53:01 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ version = cmip6_preprocessing.__version__.split("+")[0]
 # The full version, including alpha/beta/rc tags.
 release = cmip6_preprocessing.__version__
 
+print("package version:",  version)
+
 
 # -- Project information -----------------------------------------------------
 
