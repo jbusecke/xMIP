4b4904ff5e013e7ba59fbbec4a1c876f78fe4f75
Julius Busecke <julius@ldeo.columbia.edu>
2021-06-24 14:08:00 -0400

    Fix typo and formatting [skip-ci]

--- cmip6_preprocessing/__init__.py
+++ cmip6_preprocessing/__init__.py
@@ -1,10 +1,16 @@
 try:
-    from importlib.metadata import version, PackageNotFoundError # only works for python 3.8 and upwards
+    from importlib.metadata import (
+        version, 
+        PackageNotFoundError
+    ) # only works for python 3.8 and upwards
 except:
-    from importlib_metadata import version, PackageNotFoundError # works for python <3.8
+    from importlib_metadata import (
+        version,
+        PackageNotFoundError
+    ) # works for python <3.8
 
 try:
-    __version__ = version("version_testing")
+    __version__ = version("cmip6_preprocessing")
 except PackageNotFoundError:
     # package is not installed
     __version__ = "unknown"
