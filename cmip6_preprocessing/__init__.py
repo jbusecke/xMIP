22248ccfc03cdb17efe3443cd4388b7ab7fdb94f
Julius Busecke <julius@ldeo.columbia.edu>
2021-06-24 14:34:54 -0400

    Update __init__.py (#153)
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Implement fallback for earlier python version [skip-ci]
    
    * Fix typo and formatting [skip-ci]
    
    * Ughhh black, com on [skip-ci]
    
    * I really hate not having black auto format stuff here...[skip-ci]
    
    * another reformatting, this time pre-commit locally

--- cmip6_preprocessing/__init__.py
+++ cmip6_preprocessing/__init__.py
@@ -1,4 +1,17 @@
 try:
-    from ._version import __version__
-except ImportError:
+    from importlib.metadata import (
+        version,
+        PackageNotFoundError,
+    )  # only works for python 3.8 and upwards
+except:
+    from importlib_metadata import (
+        version,
+        PackageNotFoundError,
+    )  # works for python <3.8
+
+try:
+    __version__ = version("cmip6_preprocessing")
+except PackageNotFoundError:
+    # package is not installed
     __version__ = "unknown"
+    pass
