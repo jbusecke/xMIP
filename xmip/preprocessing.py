844bae0abe08aad18c8342454b2cf0e0bea8f5db
jbusecke <julius@ldeo.columbia.edu>
2023-01-09 22:08:43 -0500

    fix

--- xmip/preprocessing.py
+++ xmip/preprocessing.py
@@ -73,7 +73,7 @@ def rename_cmip6(ds, rename_dict=None):
             for target, candidates in rdict.items():
                 if di in candidates:
                     da = da.swap_dims({di: target})
-                    if di in da.variables:
+                    if di in da.coords:
                         da = da.drop_vars(di)
         return da
 
