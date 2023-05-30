2b15ff9555492afe318cd818fef9e05aa6f3a0b2
Joran Angevaare <joranangevaare@gmail.com>
2023-05-30 15:52:55 +0200

    assume longitude is cyclic

--- xmip/preprocessing.py
+++ xmip/preprocessing.py
@@ -137,7 +137,9 @@ def broadcast_lonlat(ds, verbose=True):
 def _interp_nominal_lon(lon_1d):
     x = np.arange(len(lon_1d))
     idx = np.isnan(lon_1d)
-    return np.interp(x, x[~idx], lon_1d[~idx], period=360)
+    # Assume that longitudes are cyclic
+    ret =  np.interp(x, x[~idx], lon_1d[~idx], period=len(lon_1d))
+    return ret
 
 
 def replace_x_y_nominal_lat_lon(ds):
