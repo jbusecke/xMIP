8964e07c14f7444098d7c428735e5c35a4a3cbea
Julius Busecke <julius@ldeo.columbia.edu>
2024-06-25 00:25:10 -0400

    Update utils.py

--- xmip/utils.py
+++ xmip/utils.py
@@ -60,6 +60,7 @@ def model_id_match(match_list, id_tuple):
 
 
 def _key_from_attrs(ds, attrs, sep="."):
+    raise
     return sep.join([ds.attrs[i] if i in ds.attrs.keys() else "none" for i in attrs])
 
 
@@ -67,7 +68,7 @@ cmip_instance_id_schema = "mip_era.activity_id.institution_id.source_id.experime
 
 
 def instance_id_from_dataset(
-    ds: xr.Dataset, id_schema: str = None, print_missing=True, missing_value="none"
+    ds: xr.Dataset, id_schema: str = None, print_missing=True, missing_value="none", sep=".",
 ) -> str:
     """
     Formats a CMIP6 compatible instance id from `ds` attributes according to `id_schema` (defaults to official CMIP naming schema).
@@ -84,7 +85,7 @@ def instance_id_from_dataset(
         warnings.warn(
             f"Could not find dataset attributes for facets: {list(missing_value_dict.keys())}"
         )
-    return ".".join([facet_dict[f] for f in facets])
+    return sep.join([facet_dict[f] for f in facets])
 
 
 def cmip6_dataset_id(
